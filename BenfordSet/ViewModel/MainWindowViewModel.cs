227da0571954dd85eae6db30c8e7ed7a53874b8a
mellow <stay.mellow.993@gmail.com>
2022-01-27 18:12:04 +0100

    Remove canexecute method for quit and select. KISS principle

--- BenfordSet/ViewModel/MainWindowViewModel.cs
+++ BenfordSet/ViewModel/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using BenfordSet.Common;
+using BenfordSet.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,7 @@ namespace BenfordSet.ViewModel
 {
     internal class MainWindowViewModel : ViewModelBase
     {
+        #region Fields and properties
         private string _filepath;
         public string Filepath
         {
@@ -25,6 +27,9 @@ namespace BenfordSet.ViewModel
             }
         }
 
+        private string _content;
+        public string Content { get => _content; set => _content = value; }
+
         private DelegateCommand _analyseCommand;
         private DelegateCommand _saveCommand;
         private DelegateCommand _selectCommand;
@@ -34,30 +39,24 @@ namespace BenfordSet.ViewModel
         public DelegateCommand SaveCommand { get => _saveCommand; }
         public DelegateCommand SelectCommand { get => _selectCommand; } 
         public DelegateCommand QuitCommand { get => _quitCommand; } 
+        #endregion
 
         public MainWindowViewModel()
         {
-            _selectCommand = new DelegateCommand(Select, CanSelect);
+            _selectCommand = new DelegateCommand(Select);
             _analyseCommand = new DelegateCommand(Analyse, CanAnalyse);
             _saveCommand = new DelegateCommand(Save, CanSave);
-            _quitCommand = new DelegateCommand(Quit, CanQuit);
+            _quitCommand = new DelegateCommand(Quit);
         }
 
-        private bool CanQuit() => true;
-      
 
-        private void Quit()
-        {
-            MessageBox.Show("quit");
-        }
-
+        #region Button logic
         private void Select()
         {
             SelectFile selectfile = new SelectFile();
             Filepath = selectfile.OpenDialog();
             RaisePropertyChanged();        
         }
-
 
         private void Save()
         {
@@ -66,23 +65,32 @@ namespace BenfordSet.ViewModel
 
         private void Analyse()
         {
-            MessageBox.Show("analyse");
+            var readfile = new ReadPdf(Filepath);
+            PrepareRead(readfile);
         }
 
+        private void PrepareRead(ReadPdf readfile)
+        {
+            readfile.GetFileContent();
+            RaisePropertyChanged();
+        }
+        private void Quit() => Application.Current.Shutdown();
+      
+        #endregion
+
+
         #region CanExecute mehtods
-        private bool CanAnalyse() => !string.IsNullOrWhiteSpace(Filepath) ? true : false;
-   
-        private bool CanSelect()
-            => true;
-        private bool CanSave()
-            => true;
+        private bool CanAnalyse() => !string.IsNullOrWhiteSpace(Filepath);
+        private bool CanSave() => !String.IsNullOrEmpty(Filepath) && !String.IsNullOrEmpty(Content);
+
         #endregion
 
         private void RaisePropertyChanged([CallerMemberName] string propname = "")
         {
-            ((DelegateCommand)AnalyseCommand).OnExecuteChanged();
-            ((DelegateCommand)SaveCommand).OnExecuteChanged();
-            ((DelegateCommand)SelectCommand).OnExecuteChanged();
+            SelectCommand.OnExecuteChanged();
+            AnalyseCommand.OnExecuteChanged();
+            SaveCommand.OnExecuteChanged();
+            QuitCommand.OnExecuteChanged();
         }
     }
 }
