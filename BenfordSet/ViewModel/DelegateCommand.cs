a842be3c78df7ba5041676aa09cfe051a65d2028
mellow <stay.mellow.993@gmail.com>
2022-01-27 17:01:19 +0100

    Add button commands

--- BenfordSet/ViewModel/DelegateCommand.cs
+++ BenfordSet/ViewModel/DelegateCommand.cs
@@ -11,27 +11,10 @@ namespace BenfordSet.ViewModel
     {
         private Action _execute;
         private Func<bool> _canExecute;
-
-        public DelegateCommand(Action execute ) { _execute = execute; }
-        public DelegateCommand(Action execute, Func<bool> canexecute) 
-        { 
-            _execute = execute;
-            _canExecute = canexecute;   
-        }
-
-        
         public event EventHandler? CanExecuteChanged;
-
-        
-
-        public bool CanExecute(object? parameter)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Execute(object? parameter)
-        {
-            throw new NotImplementedException();
-        }
+        public DelegateCommand(Action execute, Func<bool> canexecute) { _execute = execute; _canExecute = canexecute; }
+        public void OnExecuteChanged() => CanExecuteChanged?.Invoke(this, new EventArgs());
+        public bool CanExecute(object? parameter) => _canExecute != null ? _canExecute() : true;
+        public void Execute(object? parameter) => _execute();
     }
 }
--- BenfordSet/ViewModel/ViewModelBase.cs
+++ BenfordSet/ViewModel/ViewModelBase.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BenfordSet.ViewModel
 {
-    internal class ViewModelBase
+    internal class ViewModelBase : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propname = "")
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propname));
     }
 }
