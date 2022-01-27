c58558a290483de1d337e86ab59b26db44296cb8
mellow <stay.mellow.993@gmail.com>
2022-01-27 18:08:19 +0100

    Setup read pdf class

---
+++ BenfordSet/Model/ReadPdf.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using UglyToad.PdfPig;
+using UglyToad.PdfPig.Content;
+
+
+namespace BenfordSet.Model
+{
+    internal class ReadPdf
+    {
+        public string Filename { get; set; }
+        public int PageNumber { get; set; }
+        public string Content { get; set; }
+        public ReadPdf(string filename)
+        {
+            Filename = filename;
+        }
+
+        public void GetFileContent()
+        {
+            using PdfDocument document = PdfDocument.Open(Filename);
+            foreach (var page in document.GetPages())
+            {
+                //Content += p.Text;
+                FetchSinglePage(page); // += readFilesCompleted;
+            }
+            //Task.WaitAll(GetFileContent());
+        }
+
+        private void FetchSinglePage(Page p)
+        {
+            if (p == null)
+                throw new ArgumentNullException();
+
+            Content += p.Text;
+            PageNumber += p.Number;
+        }
+    }
+}
