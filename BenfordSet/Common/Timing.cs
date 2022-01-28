448e085d769ae4d738c422c7f0302c1a280088b0
mellow <stay.mellow.993@gmail.com>
2022-01-28 14:06:41 +0100

    Add class for time measurement

---
+++ BenfordSet/Common/Timing.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenfordSet.Common
+{
+    internal class Timing
+    {
+        public Stopwatch Stopwatch { get; set; }    
+        public void StartTimeMeasurement()
+        {
+            Stopwatch.Start();
+        }    
+        
+        public void StopTimeMeasurement()
+        {
+            Stopwatch.Stop();
+        }
+    }
+}
