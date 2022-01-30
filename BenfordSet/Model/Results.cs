d23cb5ce539711e04df94745776121965c807b90
mellow <stay.mellow.993@gmail.com>
2022-01-30 19:14:08 +0100

    Implement result output

--- BenfordSet/Model/Results.cs
+++ BenfordSet/Model/Results.cs
@@ -6,23 +6,71 @@ using System.Threading.Tasks;
 
 namespace BenfordSet.Model
 {
-    internal class Results
+    internal class Results : FileAttributes
     {
 
         public string CalculationResults { get; set; }
         public string Deviation { get; set; }
+        public double Threshold { get; set; }
+
+        public double[] BenfordNumbers { get; } = { 30.1, 17.6, 12.5, 9.7, 7.9, 6.7, 5.8, 5.1, 4.6 };
+        public double[] Digits { get; private set; } = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        public double[] Difference { get; private set; } = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        public int[] CountedNumbers { get; private set; }
+
+        private int testNumber = 5;
 
 
-        public Results(Calculation calculate) => Deviation = calculate.ListOfBenfordNumbers.ToString();
+        public Results(Calculation calcObj)
+        {
+            BenfordNumbers = calcObj.BenfordNumbers;
+            Digits = calcObj.Digits;
+            Difference = calcObj.Difference;
+            Threshold = calcObj.Threshold;
+            CountedNumbers = calcObj.CountedNumbers;
+        }
 
         public string BuildResultString()
         {
-            StringBuilder sb = new StringBuilder();
-            sb.Append(new string('#', 60));
-            sb.Append("\nResults of the benford analysis.\n");
-            sb.Append(new string('#', 60));
-            return sb.ToString();
+            var header = BuildHeader();
+            var body = BuildBody();
+            return header + body;
+        }
 
+
+        private string BuildHeader()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(new string('#', 60));
+            sb.AppendLine("Results of the benford analysis.");
+            sb.AppendLine(new string('#', 60));
+            sb.AppendLine("Filename:\t" + testNumber);
+            sb.AppendLine("All numbers:\t " + CountedNumbers.Length);
+            sb.AppendLine("Threshold:\t " + Threshold + " %");
+            sb.AppendLine();
+            sb.AppendLine("Benford Distribution \t Your Distribution \t\t Difference");
+            // trigger event, if deviation is to big
+            return sb.ToString();
+        }
+
+        private string BuildBody()
+        {
+            StringBuilder sb = new StringBuilder();
+            for (var i = 0; i < CountedNumbers.Length; i++)
+            {
+                if (Difference[i] < Threshold)
+                {
+                    string? res1 = ("{0}: {1} % \t\t {2}: {3} %  \t\t {4}: {5} %", i + 1, BenfordNumbers[i], i + 1, Digits[i], i + 1, Difference[i]).ToString();
+                    sb.AppendLine(res1);
+                }
+                else
+                {
+                    string? res2 = ("{0}: {1} % \t\t {2}: {3} %  \t\t {4}: {5} %", i + 1, BenfordNumbers[i], i + 1, Digits[i], i + 1, Difference[i]).ToString();
+                    sb.AppendLine(res2);
+                }
+            }
+            return sb.ToString();
+            //PrintDeviation(calcObj.CountDeviations);
         }
     }
 }
