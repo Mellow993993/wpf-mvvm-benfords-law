7796a6623e454504d05b16a770f42dc10f490a08
mellow <stay.mellow.993@gmail.com>
2022-01-31 12:05:06 +0100

    Debug and validate core logic

--- BenfordSet/Model/Calculation.cs
+++ BenfordSet/Model/Calculation.cs
@@ -9,22 +9,15 @@ namespace BenfordSet.Model
 {
     internal class Calculation : FileAttributes
     {
-        #region "Private Fields"
-        private int _countDeviations;
         public int NumberInFiles { get; set; }
-        #endregion
-
-        #region "Properties"
-        public int CountDeviations { get { return _countDeviations; } private set { _countDeviations = value; } }
+        public int CountDeviations { get; private set; }
         public int[] CountedNumbers { get; private set; }
         public double Threshold { get; set; }
         public double[] BenfordNumbers { get; } = { 30.1, 17.6, 12.5, 9.7, 7.9, 6.7, 5.8, 5.1, 4.6 };
-        public double[] Digits { get; private set; } = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-        public double[] Difference { get; private set; } = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-        //public List<double> ListOfDigits { get; private set; }
-        //public List<double> ListOfBenfordNumbers { get; private set; }
-        //public List<double> ListOfDifferance { get; private set; }
-        #endregion
+        public double[] Digits { get; protected set; } = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        public double[] Difference { get; protected set; } = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        public string CalculationResult { get; set; }
+
         public Calculation() { }
         public Calculation(CountNumbers countObj)
         {
@@ -39,16 +32,9 @@ namespace BenfordSet.Model
             CalculateDistribution();
             Deviation();
             ClassifyResults();
-            var output = GetOutput();
-            //CreateLists();
+            GetOutput();
         }
 
-        //private void CreateLists()
-        //{
-        //    ListOfDigits = new List<double>(Digits);
-        //    ListOfBenfordNumbers = new List<double>(BenfordNumbers);
-        //    ListOfDifferance = new List<double>(Difference);
-        //}
 
         private void CalculateDistribution()
         {
@@ -58,11 +44,13 @@ namespace BenfordSet.Model
 
         private double ConvertTypes(int numbers)
             => (double)numbers;
+
         private void Deviation()
         {
             for (var k = 0; k < BenfordNumbers.Length; k++)
                 Difference[k] = Math.Round(Math.Abs(BenfordNumbers[k] - Digits[k]), 1);
         }
+
         private void ClassifyResults()
         {
             for (int i = 0; i <= BenfordNumbers.Length - 1; i++)
@@ -70,34 +58,37 @@ namespace BenfordSet.Model
                     CountDeviations += 1;
         }
 
-        private string GetOutput()
+        private void GetOutput()
         {
-            Output output = new Output();
-            return output.BuildAnalyseResult();
+            Output output = new Output(CountedNumbers.Length, Digits, Difference, Threshold);
+            CalculationResult = output.BuildAnalyseResult();
         }
 
     }
 
      class Output : Calculation
      {
-        public Output() { }
+        private int _length;
+        public Output(int length, double[] digits, double[] difference, double threshold) 
+        {
+            _length = length;
+            Digits = digits;
+            Difference = difference;
+            Threshold = threshold;
+        }
         internal string BuildAnalyseResult()
         {
             StringBuilder sb = new StringBuilder();
-            for (var i = 0; i < CountedNumbers.Length; i++)
+            for (var i = 0; i < _length; i++)
             {
                 if (Difference[i] < Threshold)
                     sb.AppendLine(CombineOutput(i));
-                    //string? res1 = CombineOutput(i);
-                    //sb.AppendLine(res1);
                 else
                     sb.AppendLine(CombineOutput(i));
-                    //string? res2 = CombineOutput(i);
-                    //sb.AppendLine(res2);
             }
             return sb.ToString();
         }
         private string CombineOutput(int i)
-            => (i + 1, BenfordNumbers[i], i + 1, Digits[i] + " %", i + 1, Difference[i] + " %").ToString();
+            => (BenfordNumbers[i] + " %\t\t\t" + Digits[i] + " %\t\t\t" + Difference[i] + " %").ToString();
     }
 }
