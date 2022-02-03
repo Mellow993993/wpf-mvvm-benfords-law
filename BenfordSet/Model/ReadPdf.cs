d3ddd5a6a6c337ebd940b045afe79e74e86eb440
mellow <stay.mellow.993@gmail.com>
2022-02-03 11:07:54 +0100

    Add read method in using body

--- BenfordSet/Model/ReadPdf.cs
+++ BenfordSet/Model/ReadPdf.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using UglyToad.PdfPig;
 using UglyToad.PdfPig.Content;
-
+using System.IO;
+using UglyToad.PdfPig.DocumentLayoutAnalysis.TextExtractor;
 
 namespace BenfordSet.Model
 {
@@ -14,10 +15,12 @@ namespace BenfordSet.Model
     {
         private string _filename = string.Empty;
         private string _content = string.Empty;
-        private int _pageNumbers = 0;
+        private int _NumberOfPages = 0;
         public string? Filename { get => _filename; set => _filename = value; }
-        public int PageNumbers { get => _pageNumbers; set => _pageNumbers = value; }
+        public int NumberOfPages { get => _NumberOfPages; set => _NumberOfPages = value; }
         public string? Content { get => _content; set => _content = value; }
+        public string OnlyFileName { get => Path.GetFileName(Filename);  }
+
     }
 
     internal class ReadPdf : FileAttributes
@@ -26,15 +29,16 @@ namespace BenfordSet.Model
         public void GetFileContent()
         {
             using PdfDocument document = PdfDocument.Open(Filename);
-            foreach (var page in document.GetPages())
-                FetchSinglePage(page);
+            {
+                foreach (var page in document.GetPages())
+                    FetchSinglePage(page);
+            }
         }
         
-
         private void FetchSinglePage(Page page)
         {
             Content += page.Text;
-            PageNumbers = page.Number;
+            NumberOfPages = page.Number;
         }
     }
 }
