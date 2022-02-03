39c132247507334480c290fe8f3706dde7bc3313
mellow <stay.mellow.993@gmail.com>
2022-02-03 11:05:08 +0100

    Remove output class from calculation class

--- BenfordSet/Model/Calculation.cs
+++ BenfordSet/Model/Calculation.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -64,30 +63,5 @@ namespace BenfordSet.Model
             CalculationResult = output.BuildAnalyseResult();
         }
 
-    }
-
-     class Output : Calculation
-     {
-        public Output(int[] countedNumbers, double[] digits, double[] difference, double threshold) 
-        {
-            CountedNumbers = countedNumbers;
-            Digits = digits;
-            Difference = difference;
-            Threshold = threshold;
-        }
-        internal string BuildAnalyseResult()
-        {
-            StringBuilder sb = new StringBuilder();
-            for (var i = 0; i < CountedNumbers.Length; i++)
-            {
-                if (Difference[i] < Threshold)
-                    sb.AppendLine(CombineOutput(i));
-                else
-                    sb.AppendLine(CombineOutput(i) + "\t####");
-            }
-            return sb.ToString();
-        }
-        private string CombineOutput(int i)
-            => (BenfordNumbers[i] + " %\t\t\t" + Digits[i] + " %\t\t\t" + Difference[i] + " %").ToString();
     }
 }
