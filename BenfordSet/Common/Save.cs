72758b92e919e80b6d3a84aba504e36999b86e46
mellow <stay.mellow.993@gmail.com>
2022-04-26 19:35:05 +0200

    Fix pdf save output. identify file extension pdf and txt

--- BenfordSet/Common/Save.cs
+++ BenfordSet/Common/Save.cs
@@ -8,6 +8,7 @@ using ceTe.DynamicPDF;
 using ceTe.DynamicPDF.Text;
 using ceTe.DynamicPDF.Forms;
 using ceTe.DynamicPDF.PageElements;
+using static System.Net.Mime.MediaTypeNames;
 
 
 namespace BenfordSet.Common
@@ -18,10 +19,11 @@ namespace BenfordSet.Common
         public event EventHandler? SaveNotSuccessful;
 
         private readonly string _initialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
-        private const string _allowedFiles = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+        private const string _allowedFilesText = "txt files (*.txt)|*.txt";
+        private const string _allowedFilesPdf = "Pdf Documents|*.pdf";
 
         public bool IsText { get; set; }
-        public string Destination { get; set; }
+        public string Destination { get; set; } = null!;
         public string OutputResult { get; set; }
         public Messages Messages { get => new Messages(); }
 
@@ -36,11 +38,15 @@ namespace BenfordSet.Common
         {
             SaveFileDialog saveFileDialog = new();
             saveFileDialog.InitialDirectory = _initialDirectory;
-            saveFileDialog.Filter = _allowedFiles;
+            saveFileDialog.Filter = SetFileExtension(); 
 
             if (saveFileDialog.ShowDialog() == DialogResult.OK)
                 Destination = saveFileDialog.FileName;
         }
+
+        private string SetFileExtension()
+            => IsText ? _allowedFilesText : _allowedFilesPdf;
+
         public void SaveFile()
         {
             if(IsText && CheckDestination())
@@ -67,7 +73,7 @@ namespace BenfordSet.Common
             Page page = new Page(PageSize.A4, PageOrientation.Portrait);
             doc.Pages.Add(page);
             ceTe.DynamicPDF.PageElements.Label label = new
-            ceTe.DynamicPDF.PageElements.Label(OutputResult, 0, 0, 504, 100, Font.Helvetica, 10, TextAlign.Left);
+            ceTe.DynamicPDF.PageElements.Label(OutputResult, 0, 0, 504, 800, Font.Helvetica, 14, TextAlign.Left);
             page.Elements.Add(label);
             doc.Draw(Destination);
         }
