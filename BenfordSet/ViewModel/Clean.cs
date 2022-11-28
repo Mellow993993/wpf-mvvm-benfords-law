343ca03e705dc587450a48336cc6f22094c62a78
mellow <stay.mellow.993@gmail.com>
2022-11-28 22:34:51 +0100

    Remove clean method

--- BenfordSet/ViewModel/Clean.cs
+++ BenfordSet/ViewModel/Clean.cs
@@ -5,21 +5,6 @@ namespace BenfordSet.ViewModel
 {
     internal class Clean
     {
-        //internal Clean(ref ReadPdf read, ref CountNumbers count, ref Calculation calc, ref Results result)
-        //{
-        //    read = null;
-        //    count = null;
-        //    calc = null;
-        //    result = null;
-        //    GC.Collect();
-        //}
-
-
-        //internal Clean(ref ReadPdf read)
-        //{
-        //    read = null;
-        //    GC.Collect();
-        //}
         internal void DisposeReadObject(ref ReadPdf? readpdf)
         {
             if (readpdf != null)
