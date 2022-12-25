14d43ad7d6cdba8339a321afe9cf7e5441eff6f5
mellow <stay.mellow.993@gmail.com>
2022-12-25 22:32:27 +0100

    Add new view and viewmodel for settings

---
+++ BenfordSet/ViewModel/SettingsViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenfordSet.ViewModel
+{
+    internal class SettingsViewModel : ViewModelBase
+    {
+        #region Fieleds
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Delegate Commands
+        internal DelegateCommand OkCommand { get; init; }
+        internal DelegateCommand ExitCommand { get; init; }
+        #endregion
+
+        #region Constructor
+        internal SettingsViewModel()
+        {
+            OkCommand = new DelegateCommand(Ok);
+            ExitCommand = new DelegateCommand(Exit);
+        }
+        #endregion
+
+        #region Private Methods
+        private void Ok()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Exit()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region CanExecute Methods
+
+        #endregion
+    }
+}
